47a3b011149a18dcdf817081c924bcb6948ecbd9
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-11-16 22:55:07 +0600

    working auto import

--- src/main.js
+++ src/main.js
@@ -2,4 +2,25 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+    './components', 
+    false, 
+    /Base[A-Z]\w+\.(vue|js)$/
+)
+
+const app = createApp(App);
+
+requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName)
+
+    const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+    )
+
+    app.component(componentName, componentConfig.default || componentConfig)
+})
+
+app.mount('#app')
