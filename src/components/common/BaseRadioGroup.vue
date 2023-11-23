32db2afd6eb62a9f06ca492cf759cf3bbff3df33
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-11-23 22:37:26 +0600

    working base group radio

---
+++ src/components/common/BaseRadioGroup.vue
@@ -0,0 +1,23 @@
+<template>
+    <BaseRadio 
+        v-for="option in options"
+        :key="option.value"
+        :label="option.label"
+        :value="option.value"
+        :name="name"
+    />
+</template>
+<script>
+export default {
+    props: {
+        options: {
+            type: Array,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
